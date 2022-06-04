fd7fe6195a9283b799376e176cfc9cc9c60d9d67
d-murphy <murphy.dan.s@gmail.com>
2022-06-04 01:02:21 -0400

    added first fetch

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 ### Articles referenced: 
 
 [Starting react app](https://dev.to/ruppysuppy/create-react-app-from-scratch-like-a-pro-de0)
+[Adding TS to react app](https://blog.logrocket.com/using-typescript-with-react-tutorial-examples/)
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 import * as React from "react";
+import { useEffect } from "react";
 
 export default function App() {
+
+    const fetchTournaments = () => {
+        return fetch("http://localhost:4400/tournaments/getTournaments")
+            .then((resp) => console.log(resp.json()))
+            .then((data) => console.log(data))
+            .catch((err) => console.error(err))
+    }
+
+    useEffect(() => {
+        console.log('hiya')
+        fetchTournaments(); 
+    }, []); 
+
     return (
         <div>
             <h1>NYS Drill Teams</h1>
