11f1bb306ecaf8d52060d6ef3dc5ff3d9b49e239
Dan Murphy <dmurphy@Dans-Air.home>
2023-01-10 01:23:45 -0500

    adding images to big 8

--- src/Components/Big8Contest.tsx
+++ src/Components/Big8Contest.tsx
@@ -18,24 +18,35 @@ export default function Big8Contest(props:Big8ContestProp) {
     const navigate = useNavigate(); 
 
      return (
-        <div className="col big8-bg rounded m-1 px-2 pt-3 pb-1 d-flex flex-column align-items-start justify-content-start text-center big8-contest"
-            onClick={() => {
+        <div className="col-xs-12 col-sm-3 col-xxl">
+            <div className="big8-bg rounded d-flex flex-column align-items-center justify-content-start text-center big8-contest py-1 px-2 h-100" onClick={() => {
                 if(run.tournamentId) navigate(`/Tournament/${run.tournamentId}`); 
-            }}    
-        >
-            <h5 className="text-start">{run?.contest ? run.contest == 'Three Man Ladder' ? '3 Man Ladder' : run.contest : ''}</h5>
-            <h4 className="mb-2">{run?.time ? run.time : ''}</h4>
-            <div className="d-flex flex-row align-items-center justify-content-center mb-3">
-                <div className="text-start">
+            }}> 
+                <h5 className="text-center">{run?.contest ? run.contest == 'Three Man Ladder' ? '3 Man Ladder' : run.contest : ''}</h5>
+                <div className="d-flex flex-column align-items-center justify-content-center">
+                    <div className="row">
+                        <div className="col-5 col-sm-2 col-md-3 col-lg-4 col-xxl-3"></div>
+                        <div className="col-2 col-sm-8 col-md-6 col-lg-4 col-xxl-6 height-70 d-flex align-items-center justify-content-center">{run?.team ? <Image src={getImgLocation(run.team)} fluid={true} /> : <></>}</div>
+                        <div className="col-5 col-sm-2 col-md-3 col-lg-4 col-xxl-3"></div>
+                    </div>
                     <div className="font-small">
                         {run?.team ? `${run.team}` : <Placeholder animation="glow" className="w-100" />}
                     </div>
-                    <div className="text-start font-xx-small ">{run?.tournament ? `@ ${run.tournament}` : <Placeholder animation="glow" className="w-100" />}</div>
                 </div>
-                <div className="w-75">{run?.team ? <Image src={getImgLocation(run.team)} fluid={true} /> : <Placeholder animation="glow" className="w-100"/>}</div>
-            </div>
-            <div className="text-center mt-auto font-small ">
-                {dateStr ? `${dateStr}` : <Placeholder animation="glow" className="w-100" />} 
+                <div className="flex-grow-1 minheight-10"></div>
+                <div>
+                    <div className="d-flex flex-row align-items-end justify-content-center ">
+                        <div className="d-flex flex-column align-items-start mb-1">
+                            <div className="font-xx-small text-start">
+                                {run?.tournament ? `${run.tournament}` : <Placeholder animation="glow" className="w-100" />}
+                            </div>
+                            <div className="font-xx-small text-start">
+                                {dateStr ? `${dateStr}` : <Placeholder animation="glow" className="w-100" />}
+                            </div>
+                        </div>
+                        <h4 className="ms-2">{run?.time ? run.time : ''}</h4>
+                    </div>
+                </div>
             </div>
         </div>
         )
