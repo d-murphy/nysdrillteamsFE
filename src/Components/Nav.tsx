e99350689e21cf89edc0d523cd5fcb8aa8ebc123
Dan Murphy <murphy.dan.s@gmail.com>
2024-07-24 21:12:03 -0400

    adding nav elm

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -75,6 +75,9 @@ export default function Nav() {
                                         dropdownOpen ? 
                                         <div className="position-absolute dropdown-pos-top start-25 bg-white border border-1 rounded py-2 px-4" onClick={() => setDropdownOpen(false)}>
                                             <div className="my-2">
+                                                <Link className="video-links " to="/locations">Locations</Link>
+                                            </div>
+                                            <div className="my-2">
                                                 <Link className="video-links " to="/TeamSummaries">Team Seasons</Link>
                                             </div>
                                             <div className="my-2">
